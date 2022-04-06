713c61290225b25f42e440ce9c80b047341a9d5c
oussama <oussamadabachilpro@gmail.com>
2022-04-06 17:10:06 +0200

    js jqueryou

--- app/app.js
+++ app/app.js
@@ -1,8 +1,70 @@
 document.addEventListener("DOMContentLoaded",()=>{
     let pageH = document.querySelector(".page_hide")
     let buttonMenu = document.querySelector(".center")
+    let butt_sumbit_pc = document.querySelector(".btn_submit_pc")
     buttonMenu.addEventListener('click',()=>{
         pageH.classList.toggle("page_show")
 
     })
+    let email_test = "aza@gmail.com"
+    let mdp_test = "toor"
+
+const User = {
+
+    mail_user_pc : document.querySelector(".email_input").value,
+    pwd_user_pc  : document.querySelector(".pwd_input").value  , 
+
+
+    mail_user_mob : document.querySelector(".email_input_mob").value,
+    pwd_user_mob  : document.querySelector(".pwd_input_mob").value   
+
+}
+   
+    let btn_submit_pc_js = document.querySelector(".btn_submit_pc")
+    let btn_submit_mob_js = document.querySelector(".btn_submit_mob")
+
+    btn_submit_pc_js.addEventListener("click",(event)=>{
+
+        event.preventDefault()
+    if (mdp_test === User.pwd_user_pc && email_test === User.mail_user_pc) {
+        Swal.fire(
+            'Bienvenu!',
+            'Dans votre espace!',
+            'success'
+          )
+          
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Mais...',
+            text: 'Votre adresse mail ou votre mot de passe sont incorrects',
+            footer: '<a href="">Pourquoi jai ce pb</a>'
+          })
+          
+          
+    }
+})
+
+btn_submit_mob_js.addEventListener("click",(event)=>{
+
+    event.preventDefault()
+if (mdp_test === User.pwd_user_mob && email_test == User.mail_user_mob) {
+    Swal.fire(
+        'Bienvenu!',
+        'Dans votre espace!',
+        'success'
+      )
+      
+} else {
+    Swal.fire({
+        icon: 'error',
+        title: 'Mais...',
+        text: 'Votre adresse mail ou votre mot de passe sont incorrects',
+        footer: '<a href="">Pourquoi jai ce pb</a>'
+      })
+      
+      
+}
+})
+
 })
